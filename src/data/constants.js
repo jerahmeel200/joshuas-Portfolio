5ba50a3e859f398e116c9842c2caa8b40a9a6684
areaboxer <jamicojerahmeel@gmail.com>
2024-04-19 22:31:46 +0100

    update

--- src/data/constants.js
+++ src/data/constants.js
@@ -13,8 +13,8 @@ import translator from "../images/translator.png";
 import fgc from "../images/fgc.png";
 
 export const Bio = {
-  name: "Joshua Arkson",
-  roles: ["UI/UX Designer"],
+  name: "Ikechukwu Arkson (He/Him)",
+  roles: ["Product Manager | UI/UX Designer"],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com ",
